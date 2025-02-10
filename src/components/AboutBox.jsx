026d6a52426c704dcda9ff5c6c1d03a30fc86a2b
jonghwan3 <pjhan93@hanyang.ac.kr>
2025-02-09 23:37:06 -0800

    fullstack

--- src/components/AboutBox.jsx
+++ src/components/AboutBox.jsx
@@ -7,13 +7,16 @@ const AboutBox = () => {
         <h1>About</h1>
         <div className="about-box">
             <div className="about">
-                <p>Hello! I am Jonghwan Park. Software Developer and an AI Researcher.</p>
+                <p>Hello! I am Jonghwan Park, Software Developer and AI Researcher.</p>
                 <p>
-                I have over 2 years of experience working as a software developer, 
-                as well as over 2 years of AI research experience.
+                I have 2+ years of experience in REST APIs and full-stack development. 
+                Also, I have 2+ years of AI research in distributed learning, generative modeling, and LLMs. 
                 </p>
                 <p>
-                These days, I am developing an AI chatbot at a startup.
+                Specifically, I maintained a platform with 300K+ daily users and published 5 ML papers, including 3 as the first author.
+                </p>
+                <p>
+                I am currently developing an AI chatbot at a startup.
                 </p>
             </div>
         </div>
--- src/components/SkillBox.jsx
+++ src/components/SkillBox.jsx
@@ -4,19 +4,36 @@ import "./SkillBox.scss"; // Import SCSS file
 const SkillBox = () => {
   return (
     <div id="skills" className="item-container">
-        <h1>Skills</h1>
-        <div className="skill-box">
-            <h2>Frontend</h2>
-            <video autoPlay loop muted className="skill-video">
-                <source src="frontend.mov" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-            <h2>Backend</h2>
-            <video autoPlay loop muted className="skill-video">
-                <source src="frontend.mov" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+        <h1>Full stack</h1>
+        <div className="skills-box">
+            <div className="skill-box">
+                <h2>Online marketplace</h2>
+                <video autoPlay loop muted className="skill-video">
+                    <source src="fullstack.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                <h3>
+                    Developed a full stack application.
+                    <br></br>
+                    Skills: React, Typescript, Go, Python, GraphQL, PostgreSQL
+                </h3>
+            </div>
         </div>
+        <h1>Frontend</h1>
+            <div className="skills-box">
+                <div className="skill-box">
+                    <h2>Full stack</h2>
+                    <video autoPlay loop muted className="skill-video">
+                        <source src="frontend.mov" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                    <h3>
+                        Skills: React, Typescript, Go, Python, GraphQL, PostgreSQL
+                        <br></br>
+                        Developed a product introduction homepage.
+                    </h3>
+                </div>
+            </div>
     </div>
   );
 };
